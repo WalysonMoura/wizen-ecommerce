57f5420d6bb977ec837ecf16713d98954aba1d58
{ Walyson } <walysonmoura222@gmail.com>
2023-05-26 20:09:12 -0300

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -43,7 +43,7 @@ module.exports = {
         border: "hsl(214.3 31.8% 91.4%)",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
-        //background: "hsl(0 0% 100%)",
+        background: "hsl(0 0% 100%)",
         foreground: "hsl(222.2 47.4% 11.2%)",
 
         primary: {
