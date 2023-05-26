575a8fb2bafccbfbadebb8dd9e8b86e2dc43dab5
{ Walyson } <walysonmoura222@gmail.com>
2023-05-26 19:33:09 -0300

    new

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,6 @@
 const { fontFamily } = require("tailwindcss/defaultTheme");
 
-const defaultColors = {
-  primary: {
-    lightBlue: "#009ED0",
-    white: "#f8f8f8",
-    darkBlue: "#051932",
-    DEFAULT: "hsl(var(--primary))",
-    foreground: "hsl(210 40% 98%))",
-  },
-  background: {
-    header: "#051932",
-    informationBar: "#009ED0",
-    page: "hsl(0 0% 100%)",
-  },
-  colorDefaultMenuIcon: "rgba(57, 57, 57, 0.87)",
-  colorActiveMenuIcon: "#028cb8",
-
-  white1: "#fff",
-  white2: "#f9fbfb",
-  white3: "#E3F3E9",
-
-  black: "#0c0d0e",
-  gray900: "#121214",
-  gray800: "#202024",
-  gray300: "#c4c4cc",
-  gray100: "#e1e1e6",
-
-  blue100: "#7ccce4",
-  blue150: "#5cbede",
-  blue200: "#009ED0",
-  blue300: "#028cb8",
-  blue400: "#047ca4",
-  blue450: "#04759a",
-  blue500: "#0C5070",
-  blue800: "#051932",
-  blue900: "#0E1526",
-
-  shadow900: "hsla(174,63%, 15%, 0.15)",
-};
+const defaultColors = {};
 
 module.exports = {
   content: [
@@ -55,7 +18,42 @@ module.exports = {
         },
       },
       colors: {
-        ...defaultColors,
+        primary: {
+          lightBlue: "#009ED0",
+          white: "#f8f8f8",
+          darkBlue: "#051932",
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(210 40% 98%))",
+        },
+        background: {
+          header: "#051932",
+          informationBar: "#009ED0",
+          page: "hsl(0 0% 100%)",
+        },
+
+        white1: "#fff",
+        white2: "#f9fbfb",
+        white3: "#E3F3E9",
+
+        black: "#0c0d0e",
+        gray900: "#121214",
+        gray800: "#202024",
+        gray300: "#c4c4cc",
+        gray100: "#e1e1e6",
+
+        blue100: "#7ccce4",
+        blue150: "#5cbede",
+        blue200: "#009ED0",
+        blue300: "#028cb8",
+        blue400: "#047ca4",
+        blue450: "#04759a",
+        blue500: "#0C5070",
+        blue800: "#051932",
+        blue900: "#0E1526",
+
+        shadow900: "hsla(174,63%, 15%, 0.15)",
+        colorDefaultMenuIcon: "rgba(57, 57, 57, 0.87)",
+        colorActiveMenuIcon: "#028cb8",
 
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
