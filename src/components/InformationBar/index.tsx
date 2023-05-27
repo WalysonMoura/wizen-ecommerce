d649dc07a99a408923db351b70bce11563d434d6
{ Walyson } <walysonmoura222@gmail.com>
2023-05-26 21:08:37 -0300

    new

--- src/components/InformationBar/index.tsx
+++ src/components/InformationBar/index.tsx
@@ -2,14 +2,13 @@ import { MdPix } from "react-icons/md";
 
 export function InformationBar() {
   return (
-    <div class="h-6 w-full bg-informationBar text-white2 p-2 flex justify-center items-center">
+    <div class="h-10 w-full bg-informationBar text-white2 p-2 flex justify-center items-center">
       <span class="font-bold italic">
         Oferta Imperdível:
         <strong class="text-primaryDarkBlue">Economize 5%</strong>
         com Pagamentos em
-        <strong class="text-primaryDarkBlue">
-          PIX! <MdPix size={24} />
-        </strong>
+        <strong class="text-primaryDarkBlue">  PIX! <MdPix size={24} /> </strong>
+
       </span>
     </div>
   );
--- src/components/header/DesktopMenu/menuNav.tsx
+++ src/components/header/DesktopMenu/menuNav.tsx
@@ -2,19 +2,8 @@
 
 import * as React from "react";
 import Link from "next/link";
-import * as Style from "./styles";
-//import { getPublishedBlogPosts } from "@/services/notion"
-//import { useQuery } from "react-query"
 
-//import { BlogPost } from "@/types/blogPost"
-
-//import { getPublishedBlogPosts } from "@/services/notion"
-//import { useQuery } from "react-query"
-
-//import { BlogPost } from "@/types/blogPost"
 import { cn } from "@/lib/utils";
-
-import { Icons } from "../../icons";
 import {
   NavigationMenu,
   NavigationMenuContent,
@@ -23,13 +12,10 @@ import {
   NavigationMenuList,
   NavigationMenuTrigger,
   navigationMenuTriggerStyle,
-} from "../../ui/navigation-menu";
-
-import { BlogPost } from "@/types/blogPost";
-import { getSingleBlogPost } from "@/services/notion";
+} from "@/components/ui/navigation-menu";
+import { Icons } from "@/components/icons";
 
 import { siteConfig } from "@/config/site";
-import { MainNav } from "../main-nav";
 
 const components: { title: string; href: string; description: string }[] = [
   {
@@ -69,104 +55,72 @@ const components: { title: string; href: string; description: string }[] = [
   },
 ];
 
-export function DesktopMenuNav() {
-  // @ts-ignore
-  //const posts = await getSingleBlogPost(context.params?.slug)
-
-  //if (!posts) {
-  //  throw ''
-  //}
-
+export function NavigationMenuDemo() {
   return (
-    <>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <Link href="/" className="hidden items-center space-x-2 md:flex">
-            <Icons.fullLogo className="h-10 " />
-          </Link>
-          {siteConfig.mainNav?.length ? (
-            <>
-              {siteConfig.mainNav?.map(
-                (item, index) =>
-                  item.href && (
-                    <NavigationMenuItem key={index}>
-                      <Link href={item.href} legacyBehavior passHref>
-                        <NavigationMenuLink
-                          className={navigationMenuTriggerStyle()}
-                        >
-                          {item.title}
-                        </NavigationMenuLink>
-                      </Link>
-                    </NavigationMenuItem>
-                  )
-              )}
-            </>
-          ) : null}
+    <NavigationMenu>
+      <NavigationMenuList>
+        {siteConfig.mainNav?.length ? (
+          <>
+            {siteConfig.mainNav?.map(
+              (item, index) =>
+                item.href && (
+                  <NavigationMenuItem key={index}>
+                    <Link href={item.href} legacyBehavior passHref>
+                      <NavigationMenuLink
+                        className={navigationMenuTriggerStyle()}
+                      >
+                        {item.title}
+                      </NavigationMenuLink>
+                    </Link>
+                  </NavigationMenuItem>
+                )
+            )}
+          </>
+        ) : null}
 
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Blog Wizen</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                <li className="row-span-3">
-                  <NavigationMenuLink asChild>
-                    <Style.WizenPrimeLink href="/blog/wizen-prime">
-                      <Icons.logo className="h-8 w-8" />
-                      <div className="mb-2 mt-4 text-lg font-medium">
-                        Wizen Prime
-                      </div>
-                      <p className="text-sm leading-tight text-muted-foreground">
-                        A experiência VIP que você merece!
-                      </p>
-                    </Style.WizenPrimeLink>
-                  </NavigationMenuLink>
-                </li>
-                <ListItem href="/docs" title="Introduction">
-                  Re-usable components built using Radix UI and Tailwind CSS.
-                </ListItem>
-                <ListItem href="/docs/installation" title="Installation">
-                  How to install dependencies and structure your app.
-                </ListItem>
-                <ListItem href="/docs/primitives/typography" title="Typography">
-                  Styles for headings, paragraphs, lists...etc
-                </ListItem>
-                {/** 
-                {posts &&
-                  posts.map((post: BlogPost) => (
-                    <ListItem
-                      key={post.id}
-                      href="/docs/primitives/typography"
-                      title={post.title}
-                    >
-                      {post.description.length > 50
-                        ? `${post.description.substring(0, 50)}...`
-                        : post.description}
-                    </ListItem>
-                  ))}
-
-*/}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-
-          <NavigationMenuItem>
-            <NavigationMenuTrigger>Components</NavigationMenuTrigger>
-            <NavigationMenuContent>
-              <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
-                {components.map((component) => (
-                  <ListItem
-                    key={component.title}
-                    title={component.title}
-                    href={component.href}
+        <NavigationMenuItem>
+          <NavigationMenuTrigger>Getting started</NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+              <li className="row-span-3">
+                <NavigationMenuLink asChild>
+                  <a
+                    className="flex h-full w-full select-none flex-col justify-end rounded-md bg-gradient-to-b from-muted/50 to-muted p-6 no-underline outline-none focus:shadow-md"
+                    href="/"
                   >
-                    {component.description}
-                  </ListItem>
-                ))}
-              </ul>
-            </NavigationMenuContent>
-          </NavigationMenuItem>
-        </NavigationMenuList>
-      </NavigationMenu>
-    </>
+                    <Icons.logo className="h-6 w-6" />
+                    <div className="mb-2 mt-4 text-lg font-medium">
+                      shadcn/ui
+                    </div>
+                    <p className="text-sm leading-tight text-muted-foreground">
+                      Beautifully designed components built with Radix UI and
+                      Tailwind CSS.
+                    </p>
+                  </a>
+                </NavigationMenuLink>
+              </li>
+              <ListItem href="/docs" title="Introduction">
+                Re-usable components built using Radix UI and Tailwind CSS.
+              </ListItem>
+              <ListItem href="/docs/installation" title="Installation">
+                How to install dependencies and structure your app.
+              </ListItem>
+              <ListItem href="/docs/primitives/typography" title="Typography">
+                Styles for headings, paragraphs, lists...etc
+              </ListItem>
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+
+        <NavigationMenuItem>
+          <Link href="/docs" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              Documentation
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
   );
 }
 
