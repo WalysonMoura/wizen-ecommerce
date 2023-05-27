2f2f8c2b35b9a40c5aecefb28858a1f207260df6
{ Walyson } <walysonmoura222@gmail.com>
2023-05-26 22:48:41 -0300

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -54,7 +54,7 @@ interface RootLayoutProps {
 
 const poppins = Poppins({
   subsets: ["latin"],
-  //weight: ['100', '300', '400', '500', '700', '900'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   display: "swap",
   variable: "--font-poppins",
 })
